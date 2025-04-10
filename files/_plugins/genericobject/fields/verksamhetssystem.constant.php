aaecb7a453d2f0a5f1247db6ca51af3efc6db339
Peter Rundqvist <47314226+pete1854@users.noreply.github.com>
2025-04-10 13:49:30 +0200

    GO Verksamhetssystem add

---
+++ files/_plugins/genericobject/fields/verksamhetssystem.constant.php
@@ -0,0 +1,9 @@
+<?php
+global $GO_FIELDS, $LANG;
+
+// Systemägare
+$GO_FIELDS['users_id_systemowners_id']['name']       = $LANG['genericobject']['PluginGenericobjectVerksamhetssystem'][1];
+$GO_FIELDS['users_id_systemowners_id']['field']       = 'systemowner';
+$GO_FIELDS['users_id_systemowners_id']['input_type'] = 'dropdown'
+
+?>
